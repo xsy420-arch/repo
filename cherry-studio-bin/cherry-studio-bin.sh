f7e85015e9e337161ffc038b07039151c0bea2d3
xsy420 <xsy0420@gmail.com>
2026-01-10 08:20:47 +0800

    upgpkg: cherry-studio-bin 1.7.13-2
    fix editorconfig-checker lint in cherry-studio-bin

--- cherry-studio-bin/cherry-studio-bin.sh
+++ cherry-studio-bin/cherry-studio-bin.sh
@@ -4,7 +4,7 @@ XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
 
 # Allow users to override command-line options
 if [[ -f $XDG_CONFIG_HOME/cherry-studio-flags.conf ]]; then
-   CHERRY_STUDIO_USER_FLAGS="$(sed 's/#.*//' $XDG_CONFIG_HOME/cherry-studio-flags.conf | tr '\n' ' ')"
+	CHERRY_STUDIO_USER_FLAGS="$(sed 's/#.*//' $XDG_CONFIG_HOME/cherry-studio-flags.conf | tr '\n' ' ')"
 fi
 
 # Launch
